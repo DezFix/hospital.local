e019d810b74b59077cba2acd17a19c34b5cefa99
ELESSARX <kdephp@gmail.com>
2022-01-11 16:25:00 +0200

    0.0.1

---
+++ application/config/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+	'' => [
+		'controller' => 'main',
+		'action' => 'index',
+	],
+    [
+        'controller' => 'add',
+        'action' => 'add',
+    ],
+
+	'account/login' => [
+		'controller' => 'account',
+		'action' => 'login',
+	],
+
+	'account/register' => [
+		'controller' => 'account',
+		'action' => 'register',
+	],
+	
+];
