0f3f399c02c97aba20e3884d9ac88af8c6a60b53
Luka <worldlucky70@gmail.com>
2022-01-21 11:14:24 +0200

    Авторизация

--- application/views/account/login.php
+++ application/views/account/login.php
@@ -4,11 +4,47 @@
 	<p><input type="text" name="login"></p>
 	<p>Пароль</p>
 	<p><input type="text" name="password"></p>
-	<b><button type="submit" name="enter">Вход</button></b>
+	<b><button type="submit" name="log_in">Вход</button></b>
 </form>
 
 <?php
 
-if (isset($_POST["login"])){
-    echo $_POST["login"];
+use application\models\Users;
+use application\models\Main;
+
+//if (isset($_POST["login"]) and isset($_POST["password"])){
+//    echo $_POST["login"];
+//    echo $_POST["password"];
+//    $login = new Users();
+//    $login->enter($_POST["login"], $_POST["password"]);
+//}
+
+$login=new Users();
+
+if ($login->login()) //вызываем функцию login, которая определяет, авторизирован пользователь или нет
+
+{
+    $UID = $_SESSION['id']; //если пользователь авторизирован, присваиваем переменной $UID его id
+    $admin = is_admin($UID); //определяем, админ ли пользователь
+
 }
+else //если пользователь не авторизирован, проверяем, была ли нажата кнопка входа на сайт
+{
+    if(isset($_POST['log_in']))
+    {
+        $error = $login->enter(); //функция входа на сайт
+
+        if (count($error) == 0) //если ошибки отсутствуют, авторизируем пользователя
+        {
+            $UID = $_SESSION['id'];
+
+            $admin = is_admin($UID);
+            include ('index.php'); //подключается файл с формой
+            header("Location: http://hospital");
+            exit;
+        }
+        else print $error;
+    }
+}
+
+?>
