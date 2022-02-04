f37ca6c0fd279a0459ea9662ca89431cc3b46d81
Luka-kon <worldlucky70@gmail.com>
2022-02-04 15:43:42 +0200

    Создал страничку записей на приём(точно)

---
+++ application/views/doctor/appoint.php
@@ -0,0 +1,32 @@
+
+<form method="post">
+    </br>
+    <h3>Номер телефона:</h3>
+    <input type="text" name="phone" placeholder="380"></br>
+    <h3>Дата:</h3>
+    <input type="date" name="date"></br>
+    <h3>Время:</h3>
+    <input type="time" name="time"></br>
+    <h3>ID доктора:</h3>
+    <input type="text" name="id_doctor"></br>
+    </br>
+    <input type="submit" name="but"></br>
+</form>
+
+
+<?php
+use application\core\Model;
+use application\lib\db;
+use application\models\Main;
+if (isset($_POST['but'])){
+$query=new Main();
+
+$date=$_POST['date'];
+$time=$_POST['time'];
+$phone=$_POST['phone'];
+$id_doctor=$_POST['id_doctor'];
+
+$result = $query->qwery("insert into appoint (date,time,phone,id_doctor)values('$date', '$time', '$phone', '$id_doctor')");
+var_dump($result);
+}
+?>
